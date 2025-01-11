7ab861eb293b8df113d42ded2f22dd785ecae350
Valentina <valentina.grinchik@celadonsoft.com>
2025-01-11 16:15:21 +0300

    support request dto fix

--- src/supportRequests/dto/supportRequest.dto.ts
+++ src/supportRequests/dto/supportRequest.dto.ts
@@ -1,10 +1,12 @@
 import { SupportRequest, SupportRequestStatus } from '@prisma/client'
+import { ValidateEnum } from '../../system/utils/validation/validate.decotators'
 
 export class SupportRequestDto implements SupportRequest {
     id: number
     userId: number
     content: string | null
 
+    @ValidateEnum(SupportRequestStatus, { enum: SupportRequestStatus, enumName: 'SupportRequestStatus' })
     status: SupportRequestStatus
 
     adminId: number | null
