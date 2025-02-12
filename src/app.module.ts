08bca62f6e3dc807cf0b0354ecc03de6a8dc04a2
Valentina <valentina.grinchik@celadonsoft.com>
2025-02-12 15:02:44 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,8 +20,6 @@ import * as winston from 'winston'
 import 'winston-daily-rotate-file'
 import { utilities as nestWinstonModuleUtilities, WinstonModule } from 'nest-winston'
 import * as path from 'path'
-import { RolesGuard } from './auth/guards/roles.guard'
-import { APP_GUARD } from '@nestjs/core'
 import { AuthModule } from './auth/auth.module'
 
 @Module({
